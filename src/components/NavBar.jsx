42a3a6e25b65b16bf204a5e8c1d89011bc289dbe
Oghenevwede Louis Umukoro <wedevilleg@gmail.com>
2024-10-03 00:34:23 +0100

    toggle navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,22 +13,42 @@ import { PiNetworkBold } from "react-icons/pi";
 import { RiBankCard2Fill } from "react-icons/ri";
 import { HiLightBulb } from "react-icons/hi";
 import { AiOutlineHeatMap } from "react-icons/ai";
-import { FaWifi } from "react-icons/fa";
+import { FaLastfmSquare, FaWifi } from "react-icons/fa";
 import { BsFillGiftFill } from "react-icons/bs";
 import { TfiWorld } from "react-icons/tfi";
 import { FaTv } from "react-icons/fa";
 import { HiOutlineCubeTransparent } from "react-icons/hi";
 import { SiTransportforlondon } from "react-icons/si";
+import { VscIssueDraft } from "react-icons/vsc";
+import { MdMoney } from "react-icons/md";
+import { FaShoppingBag } from "react-icons/fa";
 
 const NavBar=()=>{
 
     const [personal, setPersonal]=useState(false);
+    const [business, setBusiness]=useState(false);
+    const [company, setCompany]=useState(false);
+    
 
 
 
 
     const togglePersonal=()=>{
         setPersonal(!personal)
+        setBusiness(false)
+        setCompany(false)
+    }
+
+    const toggleBusiness=()=>{
+        setBusiness(!business)
+        setPersonal(false)
+        setCompany(false)
+    }
+
+    const toggleCompany=()=>{
+        setCompany(!company)
+        setBusiness(false)
+        setPersonal(false)
     }
 
     return(
@@ -42,6 +62,7 @@ const NavBar=()=>{
                 <ul className="flex items-center gap-8 ml-[10rem] text-purple-900">
                     <li onClick={togglePersonal} className="flex items-center gap-2 cursor-pointer">Personal <MdArrowDropDown /></li>
 
+                    {/* toggle personal */}
                     {personal===true && <div className="absolute top-[4rem]  flex bg-white h-screen">
                         <div className="bg-red-100 h-screen px-6 py-3 rounded-[5px]">
                             <ul className="text-sm text-black flex flex-col gap-4 cursor-pointer">
@@ -56,8 +77,8 @@ const NavBar=()=>{
 
 
                         <div className="bg-neutral-100 h-screen px-8 py-3 rounded-[5px]">
-                            <h1 className="text-neutral-500 text-[12px]">Payment</h1>
-                            <ul className="text-sm text-black flex flex-col gap-4 cursor-pointer">
+                            <h1 className="text-neutral-500 text-[12px] pt-2">PAYMENTS</h1>
+                            <ul className="text-sm text-black flex flex-col gap-4 cursor-pointer pb-5">
                                 <li className="flex items-center gap-2"><span className="bg-red-50 p-2 rounded-full text-red-300"><HiLightBulb /></span>Electricity</li>
                                 <li className="flex items-center gap-2"><span className="bg-green-50 p-2 rounded-full text-green-300"><AiOutlineHeatMap /></span>Airtime</li>
                                 <li className="flex items-center gap-2"> <span className="bg-sky-50 p-2 rounded-full text-sky-300"><FaWifi /></span>Internet</li>
@@ -67,11 +88,11 @@ const NavBar=()=>{
                                 <li className="flex items-center gap-2"><span className="bg-red-50 p-2 rounded-full text-red-300"><HiOutlineCubeTransparent /></span>Betting</li>
                                 <li className="flex items-center gap-2"><span className="bg-green-50 p-2 rounded-full text-green-300"><SiTransportforlondon /></span>Transport</li>
                             </ul>
-                            <h1 className="text-neutral-500 text-[12px]">CREDIT</h1>
-                            <ul className="text-sm text-black">
-                                <li>Overdrafts</li>
-                                <li>Salary Loan</li>
-                                <li>Loan</li>
+                            <h1 className="text-neutral-500 text-[12px] pt-2">CREDIT</h1>
+                            <ul className="text-sm text-black flex flex-col gap-4 cursor-pointer">
+                                <li className="flex items-center gap-2"><span className="bg-sky-50 p-2 rounded-full text-sky-300"><VscIssueDraft /></span>Overdrafts</li>
+                                <li className="flex items-center gap-2"><span className="bg-yellow-50 p-2 rounded-full text-yellow-300"><MdMoney /></span>Salary Loan</li>
+                                <li className="flex items-center gap-2"><span className="bg-purple-50 p-2 rounded-full text-purple-300"><FaShoppingBag /></span>Loan</li>
                             </ul>
                         </div>
 
@@ -79,13 +100,52 @@ const NavBar=()=>{
                     </div>}
         
 
+                    <li onClick={toggleBusiness} className="flex items-center gap-2 cursor-pointer">Business <MdArrowDropDown /></li>
+                        {/* toggle business */}
+                        {business===true && <div className="absolute top-[4rem]  flex bg-white h-screen">
+                            <div className="bg-white h-screen px-6 py-3 rounded-[5px]">
+                                <ul className="text-sm text-black flex flex-col gap-4 cursor-pointer">
+                                    <li className="flex items-center gap-2"> <span className="bg-neutral-50 p-2 rounded-full"><RiHome4Fill /></span>Discover Business</li>
+                                    <li className="flex items-center gap-2"><span className="bg-green-50 p-2 rounded-full text-green-300"><AiOutlineHeatMap /></span>Business Registration</li>
+                                    <li className="flex items-center gap-2"><span className="bg-red-50 p-2 rounded-full text-red-300"><HiLightBulb /></span>softPOS</li>
+                                    <li className="flex items-center gap-2"> <span className="bg-sky-50 p-2 rounded-full text-sky-300"><FaWifi /></span>Kuda Payroll</li>
+                                    <li className="flex items-center gap-2"><span className="bg-slate-50 p-2 rounded-full text-slate-300"><BsFillGiftFill /></span>Cashback</li>
+                                    <li className="flex items-center gap-2"><span className="bg-orange-50 p-2 rounded-full text-orange-300"><TfiWorld /></span>POS MAchine</li>
+                                    <li className="flex items-center gap-2"><span className="bg-blue-50 p-2 rounded-full text-blue-300"><FaTv /></span>Invoicing</li>
+                                    <li className="flex items-center gap-2"><span className="bg-red-50 p-2 rounded-full text-red-300"><HiOutlineCubeTransparent /></span>Salary Loan</li>
+                                    <li className="flex items-center gap-2"><span className="bg-purple-50 p-2 rounded-full text-purple-300"><FaShoppingBag /></span>Business API</li>
+                                    <li className="flex items-center gap-2"><span className="bg-yellow-50 p-2 rounded-full text-yellow-300"><MdMoney /></span>Payroll with Bento</li>
+                                    <li className="flex items-center gap-2"><span className="bg-sky-50 p-2 rounded-full text-sky-300"><VscIssueDraft /></span>Sub Accounts</li>
+                                    <li className="flex items-center gap-2"><span className="bg-green-50 p-2 rounded-full text-green-300"><AiOutlineHeatMap /></span>Virtual POS</li>
+                                </ul>
+                            </div>
 
 
 
+                            <div className="bg-neutral-100 h-screen px-8 py-3 rounded-[5px]">
+                                <h1 className="text-neutral-500 text-[12px] pt-2">PAYMENTS</h1>
+                                <ul className="text-sm text-black flex flex-col gap-4 cursor-pointer pb-5">
+                                    <li className="flex items-center gap-2"> <span className="bg-green-50 p-2 rounded-full text-green-300"><MdMoney /></span>Send Money</li>
+                                    <li className="flex items-center gap-2"> <span className="bg-blue-50 p-2 rounded-full text-blue-300"><FaTv /></span>TV</li>
+                                    <li className="flex items-center gap-2"> <span className="bg-pink-50 p-2 rounded-full text-pink-300"><FaWifi /></span>Airtime & Internet Data</li>
+                                    <li className="flex items-center gap-2"> <span className="bg-orange-50 p-2 rounded-full text-orange-300"><HiLightBulb /></span>Electricity</li>
+                                    <li className="flex items-center gap-2"> <span className="bg-red-50 p-2 rounded-full text-red-300"><FaPiggyBank /></span>Bill Payments</li>
+                                </ul>
+                            </div>
+                        </div> }
+                        
+                    <li onClick={toggleCompany} className="flex items-center gap-2 cursor-pointer">Company <MdArrowDropDown /></li>
+                    {/* company toggle */}
+                    {company===true && <ul className="absolute top-[4rem] right-[42rem] bg-zinc-200 py-6 px-3 text-sm flex flex-col gap-4 rounded-[6px] text-black">
+                        <li className="cursor-pointer">Blog</li>
+                        <li className="cursor-pointer">Press</li>
+                        <li className="cursor-pointer">Join Our Team</li>
+                        <li className="cursor-pointer">About Us</li>
+                    </ul> }
+                    
 
-                    <li className="flex items-center gap-2">Business <MdArrowDropDown /></li>
-                    <li className="flex items-center gap-2">Company <MdArrowDropDown /></li>
                     <li className="flex items-center gap-2">Help <MdArrowDropDown /></li>
+
                 </ul>
 
 
